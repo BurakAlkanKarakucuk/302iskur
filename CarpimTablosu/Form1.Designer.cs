cc137f17776804886551bf01706945d5be77cbc8
Burak Alkan Karaküçük <115973026+BurakAlkanKarakucuk@users.noreply.github.com>
2022-10-17 20:51:54 +0300

    Carpim Tablosu

---
+++ CarpimTablosu/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CarpimTablosu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(128, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(90, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(266, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Bas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(71, 132);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(529, 323);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 500);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ CarpimTablosu/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarpimTablosu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int adet=0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+
+            adet = Convert.ToInt16(comboBox1.Text);
+            for (int i = 1; i <= adet; i++)
+            {
+                ListBox listBox = new ListBox();
+                listBox.Top = 0;
+                listBox.Left = ((i - 1) * 100);
+                listBox.Width = 90;
+                listBox.Height = 170;
+                panel1.Controls.Add(listBox);
+                if (i>5)
+                {
+                    listBox.Top = 190;
+                    listBox.Left = ((i - 6) * 100);
+                    listBox.Width = 90;
+                    listBox.Height = 170;
+                    panel1.Controls.Add(listBox);
+
+                }
+                listBox.Name = "ListBox" + (i.ToString());
+                //panel1.Controls.Add(listBox);
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    listBox.Items.Add((i) + "x" + j + "=" + (j) * i);
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem = "1";
+        }
+    }
+}
